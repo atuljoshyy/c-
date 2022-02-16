00b36320b21269299ec6ddf35580f86b9e00bda8
AtulJoshyy <atuljoshyy@gmail.com>
2022-02-16 09:08:07 +0530

    Revert "Revert "initial commit""
    This reverts commit ce7e747d897ce1db7fe748fa6cc07263db2592e0.

---
+++ area-of-circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float r, area;
+  cout<<"Enter radius of circle";
+  cin>>r;
+  area = 3.14f*r*r;
+  cout<<"The area of the circle is "<<area;
+  return 0;
+}
