92427f837fef98e103ff0bc7d26b5d8a4c6b910e
AtulJoshyy <atuljoshyy@gmail.com>
2022-02-20 09:21:33 +0530

    added more programs

---
+++ largest-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float x, y;
+  cout<<"Enter First Number : ";
+  cin>>x;
+  cout<<"Enter Second Number : ";
+  cin>>y;
+  if(x>y){
+    cout<<x<<" is the greater number";
+  }
+  else{
+    cout<<y<<" is the greater number";
+  }
+
+}
---
+++ positive-or-negative.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float x;
+  cout<<"Enter a Number : ";
+  cin>>x;
+  if(x>0){
+    cout<<x<<" is positive";
+  }
+  else{
+    cout<<x<<" is negative";
+  }
+
+}
